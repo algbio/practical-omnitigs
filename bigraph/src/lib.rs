020648debd4bbcb18c0524c193a920b231b2673e
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2020-05-25 13:57:47 +0300

    Remove test_type_layout and example unit test
    The test was only needed to test if the interface traits of the bigraph crate are defined as I want them too.
    I am now fairly certain that they are.
    More actual tests will be added soon.
    
    The example unit test is not needed as well.

--- bigraph/src/lib.rs
+++ bigraph/src/lib.rs
@@ -11,24 +11,4 @@ pub use index::*;
 
 #[cfg(test)]
 mod tests {
-    use crate::StaticGraph;
-
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-
-    #[test]
-    fn test_type_layout() {
-        let mut graph = petgraph::Graph::default();
-        let n1 = graph.add_node(4);
-        let n2 = graph.add_node(5);
-        graph.add_edge(n1, n2, 6);
-
-        fn print_graph<G: StaticGraph<i32, i32, usize> + std::fmt::Debug>(graph: &G) {
-            println!("{:?}", graph);
-        }
-
-        print_graph(&graph);
-    }
 }
