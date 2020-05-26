e914e9bf25881deba826dd6a41954f46fce7ff1d
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2020-05-26 09:43:24 +0300

    Fix unused x warnings in compact-genome crate

--- compact-genome/src/genome.rs
+++ compact-genome/src/genome.rs
@@ -2,7 +2,7 @@ pub trait Genome<'a>: From<&'a [u8]> + Into<Vec<u8>> + std::fmt::Display {
     fn reverse_complement(&self) -> Self;
 }
 
-fn ascii_complement(char: u8) -> Option<u8> {
+pub fn ascii_complement(char: u8) -> Option<u8> {
     match char {
         b'A' => Some(b'T'),
         b'T' => Some(b'A'),
