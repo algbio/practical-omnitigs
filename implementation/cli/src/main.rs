23b1869f48da2bc5c81ba89a6f609e4b7aa28d60
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2020-08-04 09:24:36 +0300

    Print count of WCCs and non-SC WCCs in verify command.
    Relates to #23

--- implementation/cli/src/main.rs
+++ implementation/cli/src/main.rs
@@ -5,11 +5,10 @@ extern crate error_chain;
 extern crate log;
 
 use clap::Clap;
-use colored::*;
 use error_chain::{ChainedError, ExitCode};
-use genome_graph::bigraph::ImmutableGraphContainer;
-use genome_graph::types::PetBCalm2Graph;
 use simplelog::{CombinedLogger, Config, LevelFilter, TermLogger, TerminalMode};
+
+mod verify;
 
 error_chain! {
     links {
@@ -75,57 +74,9 @@ fn run() -> Result<()> {
     info!("Hello");
 
     match &options.subcommand {
-        Command::Verify => verify(options),
+        Command::Verify => verify::verify(options),
     }?;
 
     info!("Goodbye");
-    Ok(())
-}
-
-fn verify(options: &CliOptions) -> Result<()> {
-    info!("Input file: {}", options.input);
-    info!(
-        "Output file: {}",
-        options.output.as_ref().unwrap_or(&"None".to_owned())
-    );
-
-    let genome_graph: PetBCalm2Graph =
-        genome_graph::io::bcalm2::read_bigraph_from_bcalm2_file(&options.input)?;
-
-    info!("");
-    info!("========================");
-    info!("=== Graph Statistics ===");
-    info!("========================");
-    info!("");
-    info!(
-        "{} binodes, {} biedges",
-        genome_graph.node_count() / 2,
-        genome_graph.edge_count() / 2
-    );
-    if genome_graph.node_count() % 2 != 0 || genome_graph.edge_count() % 2 != 0 {
-        error!("Internal error: uneven amount of nodes or edges in a bidirected graph");
-    }
-
-    let uncompacted_unitig_amount =
-        genome_graph::bigraph::unitigs::count_uncompacted_unitigs(&genome_graph);
-    if uncompacted_unitig_amount % 2 != 0 {
-        error!("Internal error: uneven amount of uncompacted unitigs in a bidirected graph");
-    }
-
-    let log_string = format!(
-        "{} uncompacted bidirected unitigs",
-        uncompacted_unitig_amount / 2
-    );
-    if uncompacted_unitig_amount == 0 {
-        info!("{}", log_string);
-    } else {
-        warn!("{}", log_string.yellow());
-    }
-
-    info!("");
-
-    if let Some(output) = &options.output {
-        genome_graph::io::bcalm2::write_bigraph_to_bcalm2_file(&genome_graph, output)?;
-    }
     Ok(())
 }
