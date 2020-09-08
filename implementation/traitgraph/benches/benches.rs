79a4d4f9713b6b9285c9057c76846e8dbf5d43e6
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2020-09-08 13:15:10 +0300

    Add comparative benchmarks for DFS.
    Turns out that BFS and DFS behave the same in terms of queue.
    
    Relates to #40

--- implementation/traitgraph/benches/benches.rs
+++ implementation/traitgraph/benches/benches.rs
@@ -1,7 +1,7 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use std::collections::{LinkedList, VecDeque};
 use traitgraph::algo::predefined_graphs::create_binary_tree;
-use traitgraph::algo::traversal::{BfsQueueStrategy, ForwardNeighborStrategy, PreOrderTraversal};
+use traitgraph::algo::traversal::{BfsQueueStrategy, DfsQueueStrategy, ForwardNeighborStrategy, PreOrderTraversal};
 use traitgraph::implementation::petgraph_impl;
 
 fn bench_petgraph_preorder_forward_bfs_traversal_linked_list_bintree_10(criterion: &mut Criterion) {
@@ -12,7 +12,7 @@ fn bench_petgraph_preorder_forward_bfs_traversal_linked_list_bintree_10(criterio
             &graph, root,
         );
 
-    criterion.bench_function("petgraph_linkedlist_bintree_10", |b| {
+    criterion.bench_function("petgraph_bfs_linkedlist_bintree_10", |b| {
         b.iter(|| {
             traversal.reset(root);
             for e in &mut traversal {
@@ -30,7 +30,7 @@ fn bench_petgraph_preorder_forward_bfs_traversal_linked_list_bintree_20(criterio
             &graph, root,
         );
 
-    criterion.bench_function("petgraph_linkedlist_bintree_20", |b| {
+    criterion.bench_function("petgraph_bfs_linkedlist_bintree_20", |b| {
         b.iter(|| {
             traversal.reset(root);
             for e in &mut traversal {
@@ -48,7 +48,7 @@ fn bench_petgraph_preorder_forward_bfs_traversal_vec_deque_bintree_10(criterion:
             &graph, root,
         );
 
-    criterion.bench_function("petgraph_vecdeque_bintree_10", |b| {
+    criterion.bench_function("petgraph_bfs_vecdeque_bintree_10", |b| {
         b.iter(|| {
             traversal.reset(root);
             for e in &mut traversal {
@@ -66,7 +66,79 @@ fn bench_petgraph_preorder_forward_bfs_traversal_vec_deque_bintree_20(criterion:
             &graph, root,
         );
 
-    criterion.bench_function("petgraph_vecdeque_bintree_20", |b| {
+    criterion.bench_function("petgraph_bfs_vecdeque_bintree_20", |b| {
+        b.iter(|| {
+            traversal.reset(root);
+            for e in &mut traversal {
+                black_box(e);
+            }
+        })
+    });
+}
+
+fn bench_petgraph_preorder_forward_dfs_traversal_linked_list_bintree_10(criterion: &mut Criterion) {
+    let mut graph = petgraph_impl::new::<(), ()>();
+    let root = create_binary_tree(&mut graph, 10).unwrap();
+    let mut traversal =
+        PreOrderTraversal::<_, ForwardNeighborStrategy, DfsQueueStrategy, LinkedList<_>>::new(
+            &graph, root,
+        );
+
+    criterion.bench_function("petgraph_dfs_linkedlist_bintree_10", |b| {
+        b.iter(|| {
+            traversal.reset(root);
+            for e in &mut traversal {
+                black_box(e);
+            }
+        })
+    });
+}
+
+fn bench_petgraph_preorder_forward_dfs_traversal_linked_list_bintree_20(criterion: &mut Criterion) {
+    let mut graph = petgraph_impl::new::<(), ()>();
+    let root = create_binary_tree(&mut graph, 20).unwrap();
+    let mut traversal =
+        PreOrderTraversal::<_, ForwardNeighborStrategy, DfsQueueStrategy, LinkedList<_>>::new(
+            &graph, root,
+        );
+
+    criterion.bench_function("petgraph_dfs_linkedlist_bintree_20", |b| {
+        b.iter(|| {
+            traversal.reset(root);
+            for e in &mut traversal {
+                black_box(e);
+            }
+        })
+    });
+}
+
+fn bench_petgraph_preorder_forward_dfs_traversal_vec_deque_bintree_10(criterion: &mut Criterion) {
+    let mut graph = petgraph_impl::new::<(), ()>();
+    let root = create_binary_tree(&mut graph, 10).unwrap();
+    let mut traversal =
+        PreOrderTraversal::<_, ForwardNeighborStrategy, DfsQueueStrategy, VecDeque<_>>::new(
+            &graph, root,
+        );
+
+    criterion.bench_function("petgraph_dfs_vecdeque_bintree_10", |b| {
+        b.iter(|| {
+            traversal.reset(root);
+            for e in &mut traversal {
+                black_box(e);
+            }
+        })
+    });
+}
+
+fn bench_petgraph_preorder_forward_dfs_traversal_vec_deque_bintree_20(criterion: &mut Criterion) {
+    let mut graph = petgraph_impl::new::<(), ()>();
+    let root = create_binary_tree(&mut graph, 20).unwrap();
+    let mut traversal =
+        PreOrderTraversal::<_, ForwardNeighborStrategy, DfsQueueStrategy, VecDeque<_>>::new(
+            &graph, root,
+        );
+
+    criterion.bench_function("petgraph_dfs_vecdeque_bintree_20", |b| {
         b.iter(|| {
             traversal.reset(root);
             for e in &mut traversal {
@@ -82,5 +154,9 @@ criterion_group!(
     bench_petgraph_preorder_forward_bfs_traversal_linked_list_bintree_20,
     bench_petgraph_preorder_forward_bfs_traversal_vec_deque_bintree_10,
     bench_petgraph_preorder_forward_bfs_traversal_vec_deque_bintree_20,
+    bench_petgraph_preorder_forward_dfs_traversal_linked_list_bintree_10,
+    bench_petgraph_preorder_forward_dfs_traversal_linked_list_bintree_20,
+    bench_petgraph_preorder_forward_dfs_traversal_vec_deque_bintree_10,
+    bench_petgraph_preorder_forward_dfs_traversal_vec_deque_bintree_20,
 );
 criterion_main!(benches);
