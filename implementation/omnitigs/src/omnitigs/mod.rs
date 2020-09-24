d769136d743bdf2a3f6404ab3eb5b584d945eafb
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2020-09-24 14:02:56 +0300

    Test function to compute maximal trivial omnitigs.
    Relates to #132

--- implementation/omnitigs/src/omnitigs/mod.rs
+++ implementation/omnitigs/src/omnitigs/mod.rs
@@ -123,3 +123,87 @@ pub trait TrivialOmnitigAlgorithm<Graph: StaticGraph> {
         omnitigs: Omnitigs<Graph>,
     ) -> Omnitigs<Graph>;
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::omnitigs::Omnitigs;
+    use traitgraph::implementation::petgraph_impl;
+    use traitgraph::interface::MutableGraphContainer;
+    use traitgraph::interface::WalkableGraph;
+
+    #[test]
+    fn test_compute_only_trivial_omnitigs_simple() {
+        let mut graph = petgraph_impl::new();
+        let n0 = graph.add_node(());
+        let n1 = graph.add_node(());
+        let n2 = graph.add_node(());
+        let n3 = graph.add_node(());
+        let n4 = graph.add_node(());
+        let n5 = graph.add_node(());
+        let n6 = graph.add_node(());
+        let n7 = graph.add_node(());
+        let n8 = graph.add_node(());
+        let n9 = graph.add_node(());
+        let n10 = graph.add_node(());
+        let n11 = graph.add_node(());
+        let n12 = graph.add_node(());
+        let n13 = graph.add_node(());
+        let n14 = graph.add_node(());
+        let n15 = graph.add_node(());
+        let n16 = graph.add_node(());
+        let n17 = graph.add_node(());
+        let n18 = graph.add_node(());
+        let n19 = graph.add_node(());
+        let n20 = graph.add_node(());
+
+        let e0 = graph.add_edge(n0, n1, ());
+        let e1 = graph.add_edge(n1, n2, ());
+        let e2 = graph.add_edge(n2, n3, ());
+        let e3 = graph.add_edge(n2, n4, ());
+        let e4 = graph.add_edge(n2, n5, ());
+        let e5 = graph.add_edge(n2, n6, ());
+        let e6 = graph.add_edge(n7, n0, ()); // Comes from all except n11.
+        let e7 = graph.add_edge(n8, n0, ());
+        let e8 = graph.add_edge(n9, n0, ());
+        let e9 = graph.add_edge(n10, n0, ());
+        let e10 = graph.add_edge(n3, n11, ()); // Goes to all except n7.
+        let e11 = graph.add_edge(n3, n12, ());
+        let e12 = graph.add_edge(n4, n13, ());
+        let e13 = graph.add_edge(n4, n14, ());
+        let e14 = graph.add_edge(n17, n8, ());
+        let e15 = graph.add_edge(n17, n9, ());
+        let e16 = graph.add_edge(n17, n10, ());
+        let e17 = graph.add_edge(n12, n18, ());
+        let e18 = graph.add_edge(n13, n18, ());
+        let e19 = graph.add_edge(n14, n18, ());
+        let e20 = graph.add_edge(n5, n18, ());
+        let e21 = graph.add_edge(n6, n18, ());
+        let e22 = graph.add_edge(n11, n15, ());
+        let e23 = graph.add_edge(n15, n16, ());
+        let e24 = graph.add_edge(n16, n17, ());
+        let e25 = graph.add_edge(n17, n17, ());
+        let e26 = graph.add_edge(n20, n7, ());
+        let e27 = graph.add_edge(n19, n20, ());
+        let e28 = graph.add_edge(n18, n19, ());
+        let e29 = graph.add_edge(n18, n18, ());
+
+        let maximal_trivial_omnitigs = Omnitigs::compute_trivial_only(&graph);
+        assert_eq!(
+            maximal_trivial_omnitigs,
+            Omnitigs::from(vec![
+                graph.create_edge_walk(&[e0, e1, e4, e20]),
+                graph.create_edge_walk(&[e0, e1, e5, e21]),
+                graph.create_edge_walk(&[e28, e27, e26, e6, e0, e1]),
+                graph.create_edge_walk(&[e14, e7, e0, e1]),
+                graph.create_edge_walk(&[e15, e8, e0, e1]),
+                graph.create_edge_walk(&[e16, e9, e0, e1]),
+                graph.create_edge_walk(&[e0, e1, e2, e10, e22, e23, e24]),
+                graph.create_edge_walk(&[e0, e1, e2, e11, e17]),
+                graph.create_edge_walk(&[e0, e1, e3, e12, e18]),
+                graph.create_edge_walk(&[e0, e1, e3, e13, e19]),
+                graph.create_edge_walk(&[e25]),
+                graph.create_edge_walk(&[e29]),
+            ])
+        );
+    }
+}
