434b001382bdae9f7a88f2aacc524a39763b9e98
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2020-09-29 07:18:43 +0300

    Implement crate that abstracts over sequence types.
    Relates to #161

---
+++ implementation/traitsequence/src/lib.rs
@@ -0,0 +1,7 @@
+#![warn(missing_docs)]
+//! This crate offers traits to abstract over sequence implementations.
+
+/// Implementations of this trait for stdlib types.
+pub mod implementation;
+/// The traits abstracting over a sequence.
+pub mod interface;
