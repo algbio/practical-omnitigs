c511df2e65df77e75ef350fd74aa03b8bf127bb5
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2021-01-20 15:36:09 +0200

    Fix job release after snakemake.

--- scripts/run_on_turso.sh
+++ scripts/run_on_turso.sh
@@ -9,8 +9,8 @@ source activate practical-omnitigs
 
 snakemake --profile config/turso $@ | tee run_on_turso.log
 
-squeue -o "%.18A %.18R" -u sebschmi -M carrington | awk '{if ($2 =="(JobHeldUser)"){print $1}}' | xargs -n 1 scontrol -M carrington resume
-squeue -o "%.18A %.18R" -u sebschmi -M ukko2 | awk '{if ($2 =="(JobHeldUser)"){print $1}}' | xargs -n 1 scontrol -M ukko2 resume
-squeue -o "%.18A %.18R" -u sebschmi -M vorna | awk '{if ($2 =="(JobHeldUser)"){print $1}}' | xargs -n 1 scontrol -M vorna resume
+squeue -o "%.18A %.18R" -u sebschmi -M carrington | awk '{if ($2 =="(JobHeldUser)"){print $1}}' | xargs -n 1 -r scontrol -M carrington release
+squeue -o "%.18A %.18R" -u sebschmi -M ukko2 | awk '{if ($2 =="(JobHeldUser)"){print $1}}' | xargs -n 1 -r scontrol -M ukko2 release
+squeue -o "%.18A %.18R" -u sebschmi -M vorna | awk '{if ($2 =="(JobHeldUser)"){print $1}}' | xargs -n 1 -r scontrol -M vorna release
 
 echo "Successfully created and released all jobs"
