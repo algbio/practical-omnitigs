58cdf90a978ca77009a72793244df0f8481967a8
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2021-01-20 15:22:03 +0200

    Add success log message after job release.

--- scripts/run_on_turso.sh
+++ scripts/run_on_turso.sh
@@ -10,3 +10,5 @@ source activate practical-omnitigs
 snakemake --profile config/turso $@ | tee run_on_turso.log
 
 squeue -o "%.18A %.18R" -u sebschmi | awk '{if ($2 =="(JobHeldUser)"){print $1}}' | xargs -n 1 scontrol resume
+
+echo "Successfully created and released all jobs"
