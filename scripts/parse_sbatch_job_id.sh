5af795c66cdd51ccb33f31693d4efefe9e464360
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2021-01-20 17:31:43 +0200

    Fix dependencies.

--- scripts/parse_sbatch_job_id.sh
+++ scripts/parse_sbatch_job_id.sh
@@ -6,7 +6,7 @@
 if [[ "Submitted batch job" =~ "$@" ]]; then
   echo -n ""
 else
-  deplist=$(grep -Eo '[0-9]{1,10}' <<< "$@" | tr '\n' ',' | sed 's/.$//')
+  deplist=$(grep -Eo '[0-9]{5,10}' <<< "$@" | tr '\n' ',' | sed 's/.$//' | sed 's/,/,afterok:/')
   echo "Creating job with deplist $deplist" >> logs/deplists.log
   echo -n "--dependency=afterok:$deplist"
 fi
