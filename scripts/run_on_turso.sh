345172d6aafa50c88dca7b2ee1cbd319d91317ee
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2021-01-20 15:29:36 +0200

    Release jobs per cluster.
    It does not work without specifying a cluster.

--- scripts/run_on_turso.sh
+++ scripts/run_on_turso.sh
@@ -9,6 +9,8 @@ source activate practical-omnitigs
 
 snakemake --profile config/turso $@ | tee run_on_turso.log
 
-squeue -o "%.18A %.18R" -u sebschmi | awk '{if ($2 =="(JobHeldUser)"){print $1}}' | xargs -n 1 scontrol resume
+squeue -o "%.18A %.18R" -u sebschmi -M carrington | awk '{if ($2 =="(JobHeldUser)"){print $1}}' | xargs -n 1 scontrol -M carrington resume
+squeue -o "%.18A %.18R" -u sebschmi -M ukko2 | awk '{if ($2 =="(JobHeldUser)"){print $1}}' | xargs -n 1 scontrol -M ukko2 resume
+squeue -o "%.18A %.18R" -u sebschmi -M vorna | awk '{if ($2 =="(JobHeldUser)"){print $1}}' | xargs -n 1 scontrol -M vorna resume
 
 echo "Successfully created and released all jobs"
