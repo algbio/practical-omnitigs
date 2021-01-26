d810bf9d9e8c50f953a72daab8aca8df4aea08e9
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2021-01-26 12:36:11 +0200

    Add another missing $.

--- scripts/run_on_turso.sh
+++ scripts/run_on_turso.sh
@@ -12,7 +12,7 @@ LOGDIR="logs/$(date +"%FT%X")/"
 mkdir -p "$LOGDIR"
 LATEST_LOGDIR_SYMLINK="logs/latest"
 rm -f "$LATEST_LOGDIR_SYMLINK"
-ln -sr "$LOGDIR" "LATEST_LOGDIR_SYMLINK"
+ln -sr "$LOGDIR" "$LATEST_LOGDIR_SYMLINK"
 
 echo "Storing logs in directory $LOGDIR"
 echo "Also symlinked as $LATEST_LOGDIR_SYMLINK"
