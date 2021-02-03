adb5c74010992dae314d7a286bcdc816454df238
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2021-02-03 11:25:58 +0200

    Improve slurm job status checking.

--- scripts/get_slurm_job_status.py
+++ scripts/get_slurm_job_status.py
@@ -10,12 +10,17 @@ for arg in sys.argv[1:]:
 if arg is None:
 	sys.exit("Not numeric argument given.")
 
-output = str(subprocess.check_output("sacct -j %s --format State --noheader -M all | head -1 | awk '{print $1}'" % jobid, shell=True).strip())
+states = [state.strip() for state in str(subprocess.check_output("sacct -j %s --format State --noheader -M all | head -1 | awk '{print $1}'" % jobid, shell=True).strip()).split('\n')]
 
 running_status=["PENDING", "CONFIGURING", "COMPLETING", "RUNNING", "SUSPENDED", "REVOKED", "REQUEUED", "RESIZING"]
-if "COMPLETED" in output:
+all_completed = True
+for state in states:
+	if state != "COMPLETED":
+		all_completed = False
+
+if all_completed:
   print("success")
-elif any(r in output for r in running_status):
+elif any(r in states for r in running_status):
   print("running")
 else:
   print("failed")
