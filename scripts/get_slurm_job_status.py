c2a9d44f35357afed12fbf6e15ae815064fcc9a2
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2021-02-03 11:47:24 +0200

    Hopefully fix job status checking.

--- scripts/get_slurm_job_status.py
+++ scripts/get_slurm_job_status.py
@@ -10,8 +10,7 @@ for arg in sys.argv[1:]:
 if arg is None:
 	sys.exit("Not numeric argument given.")
 
-states = [state.strip() for state in str(subprocess.check_output("sacct -j %s --format State --noheader -M all | awk '{print $1}'" % jobid, shell=True)).strip().split('\n')]
-print(states)
+states = [state.strip() for state in subprocess.check_output("sacct -j %s --format State --noheader -M all | awk '{print $1}'" % jobid, shell=True).decode(sys.stdout.encoding).strip().split('\n')]
 
 running_status=["PENDING", "CONFIGURING", "COMPLETING", "RUNNING", "SUSPENDED", "REVOKED", "REQUEUED", "RESIZING"]
 all_completed = True
