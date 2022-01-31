b7690a4ccce4661b516db87d1d6be704c06d8046
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2022-01-31 13:29:04 +0200

    Move traitgraph and traitsequence to separate repository.

--- implementation/traitgraph/src/implementation/mod.rs
+++
@@ -1,6 +0,0 @@
-/// A subgraph implementation based on bitvectors;
-pub mod bit_vector_subgraph;
-/// A subgraph implementation that allows to combine multiple subgraphs into one if they are totally ordered by the subset relation.
-pub mod incremental_subgraph;
-/// A graph implementation based on the `petgraph` crate.
-pub mod petgraph_impl;
