bf17ac5902ce63596032932aa16f471fb65b7f6e
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2022-03-02 14:42:31 +0200

    Move omnitigs crate to separate repo.

--- implementation/omnitigs/src/macrotigs/mod.rs
+++
@@ -1,7 +0,0 @@
-/// Algorithms and structures to compute and store macronodes.
-pub mod macronodes;
-/// Algorithms and structures to compute and store maximal macrotigs.
-#[allow(clippy::module_inception)]
-pub mod macrotigs;
-/// Algorithms and structures to compute and store maximal microtigs.
-pub mod microtigs;
