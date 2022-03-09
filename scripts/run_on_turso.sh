a772b58c677a8ebaeb67cbdaf08be53d487f89da
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2022-03-09 12:50:58 +0200

    Add log message.

--- scripts/run_on_turso.sh
+++ scripts/run_on_turso.sh
@@ -58,6 +58,7 @@ export TMP=$TMPDIR
 mkdir -p "$TMPDIR"
 
 # Build environments (due to a bug in snakemake, this needs to be done in a separate step for now)
+echo "Building conda environments"
 snakemake --use-conda --cores 1 --conda-create-envs-only "$@" 2>&1 | tee "$LOGDIR/run_on_turso.log"
 
 # Execute pipeline
