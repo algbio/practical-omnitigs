359672792ba5015e61bfca3cb777259909aa1dc4
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2022-10-07 12:29:00 +0300

    Improve plot.

--- scripts/create_combined_eaxmax_plot.py
+++ scripts/create_combined_eaxmax_plot.py
@@ -10,14 +10,14 @@ from os import path
 
 
 import pandas
-df = pandas.DataFrame(columns = ["experiment", "x", "y"])
+df = pandas.DataFrame(columns = ["experiment", "x", "EAxmax"])
 
 for shortname, quast_csv in zip(input_shortnames, input_quast_csvs):
-    frame = pandas.read_csv(quast_csv, names=["x", "y"])
+    frame = pandas.read_csv(quast_csv, names=["x", "EAxmax"])
     frame["experiment"] = shortname
     df = df.append(frame)
 
 import seaborn as sns
 import matplotlib.pyplot as plt
-plot = sns.lineplot(data=df, x="x", y="y", hue="experiment")
+plot = sns.lineplot(data=df, x="x", y="EAxmax", hue="experiment")
 plt.savefig(output_file)
